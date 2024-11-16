b345fea5eed22a1209e426c3ce4c3921dfe0896e
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-11-16 10:18:52 +0100

    feat: creation of the shraredKernel project.

---
+++ src/TechNovaLab.Irrigo.SharedKernel/Core/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using TechNovaLab.Irrigo.SharedKernel.Events;
+
+namespace TechNovaLab.Irrigo.SharedKernel.Core
+{
+    public abstract class EntityBase
+    {
+        private readonly List<IDomainEvent> domainEvents = [];
+
+        public List<IDomainEvent> DomainEvents => [.. domainEvents];
+
+        public void ClearDomainEvents()
+        {
+            domainEvents.Clear();
+        }
+
+        public void Raise(IDomainEvent domainEvent)
+        {
+            domainEvents.Add(domainEvent);
+        }
+    }
+}
