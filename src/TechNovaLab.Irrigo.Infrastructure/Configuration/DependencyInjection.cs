74331a353077ba3bc86fba4083f5a9ecf7c7c055
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-11-16 18:47:57 +0100

    feat: The dependency injection configuration is added.

---
+++ src/TechNovaLab.Irrigo.Infrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechNovaLab.Irrigo.Infrastructure.Configuration
+{
+    internal class DependencyInjection
+    {
+    }
+}
