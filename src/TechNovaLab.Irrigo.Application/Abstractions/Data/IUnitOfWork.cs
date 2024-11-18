d23d222f058c261e7227ae8c1fded446ec0dde01
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-11-18 19:38:25 +0100

    feat(UnitOfWork): configurar patrón UnitOfWork
    Se implementó el patrón UnitOfWork para manejar transacciones y operaciones
    de persistencia de manera más eficiente. Esto incluye:
    
    - Creación de la interfaz IUnitOfWork.
    - Implementación de la clase UnitOfWork.
    - Integración con el repositorio y contexto de base de datos.

---
+++ src/TechNovaLab.Irrigo.Application/Abstractions/Data/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace TechNovaLab.Irrigo.Application.Abstractions.Data
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CompleteAsync(CancellationToken cancellation = default);
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        Task RollbackTransactionAsync(IDbContextTransaction transaction, CancellationToken cancellationToken);
+        Task CommitTransactionAsync(IDbContextTransaction transaction, CancellationToken cancellationToken);
+        Task<bool> CreateExecutionStrategyAsync(Func<Task> operation, CancellationToken cancellationToken = default);
+    }
+}
