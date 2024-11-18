35faa40168d63db2de9178ed02f31c33a3c79783
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-11-18 18:40:01 +0100

    feat: configuración inicial para migraciones de base de datos
    - Se añade la configuración inicial de Entity Framework para gestionar las migraciones.
    - Incluye comandos para generar y aplicar migraciones en el entorno local.

---
+++ src/TechNovaLab.Irrigo.Infrastructure/Database/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TechNovaLab.Irrigo.Infrastructure.Database.Contexts
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=TechNovaLab.Irrigo.v1;Trusted_Connection=True;MultipleActiveResultSets=true";
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionBuilder.UseSqlServer(connectionString,
+                sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                });
+
+            return new ApplicationDbContext(optionBuilder.Options);
+        }
+    }
+}
