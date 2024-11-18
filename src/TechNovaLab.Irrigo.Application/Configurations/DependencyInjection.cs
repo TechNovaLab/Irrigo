04e5fd69dc3cc67548966303ab69ed4cb54e10f5
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-11-18 20:13:01 +0100

    feat: configurar inyección de dependencias en Application
    - Se agregaron los servicios de Application en la colección de servicios.
    - Se configuraron los contenedores necesarios para el proyecto.
    - Se implementaron las interfaces correspondientes en los servicios de Application.

--- src/TechNovaLab.Irrigo.Application/Configurations/DependencyInjection.cs
+++ src/TechNovaLab.Irrigo.Application/Configurations/DependencyInjection.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace TechNovaLab.Irrigo.Application.Configuration
+namespace TechNovaLab.Irrigo.Application.Configurations
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+                // (!) next configure open behaviors
+            });
+
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
+
+            return services;
+        }
     }
 }
