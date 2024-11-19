d3e7362b3161489d79ab6013b60648d501ffa44c
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-11-19 19:48:31 +0100

    feat: add DateTimeProvider for centralized date and time management

---
+++ src/TechNovaLab.Irrigo.Infrastructure/Providers/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using TechNovaLab.Irrigo.SharedKernel.Providers;
+
+namespace TechNovaLab.Irrigo.Infrastructure.Providers
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
