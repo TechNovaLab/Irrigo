ac0528f9dfabd2a73b80fe006afab6a8006343c7
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-11-24 09:49:52 +0100

    feat: ✨ Abstracciones 'messaging' creadas para manejadores de queries y commands

---
+++ src/TechNovaLab.Irrigo.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TechNovaLab.Irrigo.SharedKernel.Core;
+
+namespace TechNovaLab.Irrigo.Application.Abstractions.Messaging
+{
+    public interface ICommandHandler<in TCommand>
+        : IRequestHandler<TCommand, Result> where TCommand : ICommand;
+
+    public interface ICommandHandler<in TCommand, TResponse>
+        : IRequestHandler<TCommand, Result<TResponse>> where TCommand : ICommand<TResponse>;
+}
