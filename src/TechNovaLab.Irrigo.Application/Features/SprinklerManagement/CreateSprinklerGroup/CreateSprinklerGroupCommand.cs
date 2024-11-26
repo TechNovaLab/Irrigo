be3989130e630a778da229e19235f434861bbe11
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-11-26 19:15:43 +0100

    feat: 🌱 se ha implementado la funcionalidad para crear grupos de aspersores
    - Se añade un endpoint en la API para la creación de grupos de aspersores.
    - Documentación del endpoint en Swagger.

---
+++ src/TechNovaLab.Irrigo.Application/Features/SprinklerManagement/CreateSprinklerGroup/CreateSprinklerGroupCommand.cs
@@ -0,0 +1,6 @@
+﻿using TechNovaLab.Irrigo.Application.Abstractions.Messaging;
+
+namespace TechNovaLab.Irrigo.Application.Features.SprinklerManagement.CreateSprinklerGroup
+{
+    public sealed record CreateSprinklerGroupCommand : ICommand<SprinklerGroupResponse>;
+}
